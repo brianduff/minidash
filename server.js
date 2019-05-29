1804df18b2578ee2d4f00202b6e851126c0e3c22
Brian Duff <cairndubh@gmail.com>
2019-05-29 14:42:34 -0700

    Use a different path for the database. Reformat.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
+const os = require("os");
+const path = require("path");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-
-const loki = require("lokijs")
-const db = new loki("loki.json", {autosave: true, autosaveInterval: 5000, autoload: true});
+const loki = require("lokijs");
+const dbFilePath = path.join(os.homedir(), "loki.json");
+const db = new loki(dbFilePath, {
+  autosave: true,
+  autosaveInterval: 5000,
+  autoload: true
+});
 
 function getOrCreateCollection(collectionName) {
   var col = db.getCollection(collectionName);
@@ -28,65 +34,65 @@ function fetchSingleRecord(collectionName, searchParams) {
   return record[0];
 }
 
-var args = require('commander');
+var args = require("commander");
 
 args
-  .version('1.0')
-  .option('-p, --port <n>', 'Port number', 8080)
+  .version("1.0")
+  .option("-p, --port <n>", "Port number", 8080)
   .parse(process.argv);
 
 app
   .prepare()
   .then(() => {
-    const server = express()
+    const server = express();
 
     server.use(express.json());
 
     // Initializes (or resets) the db with some data
     server.get("/initdb", (req, res) => {
       var coins = getOrCreateCollection("GoldCoins");
-      coins.insert({name:'michael', coins: 10});
-      coins.insert({name:'caitlin', coins: 40});
+      coins.insert({ name: "michael", coins: 10 });
+      coins.insert({ name: "caitlin", coins: 40 });
 
       var dates = getOrCreateCollection("Dates");
-      dates.insert({name:"lastChipDay", value: "2019-05-10"});
+      dates.insert({ name: "lastChipDay", value: "2019-05-10" });
 
-      res.send("OK")
-    })
+      res.send("OK");
+    });
 
     server.get("/dates/:name", (req, res) => {
-      var record = fetchSingleRecord("Dates", {name: req.params.name});
+      var record = fetchSingleRecord("Dates", { name: req.params.name });
       if (record == null) {
         res.status(404).send("Not Found");
         return;
       }
       res.send(record);
-    })
+    });
 
     server.get("/goldcoins/:name", (req, res) => {
-      var record = fetchSingleRecord("GoldCoins", {name: req.params.name});
+      var record = fetchSingleRecord("GoldCoins", { name: req.params.name });
       if (record == null) {
         res.status(404).send("Not Found");
         return;
       }
       res.send(record);
-    })
+    });
 
-    server.get('/test', (req, res) => {
-      console.log("Hello")
-      return res.json({ hello: "world" })
-    })
+    server.get("/test", (req, res) => {
+      console.log("Hello");
+      return res.json({ hello: "world" });
+    });
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
     server.listen(args.port, err => {
-      if (err) throw err
-      console.log("Ready on port %s", args.port)
-    })
+      if (err) throw err;
+      console.log("Ready on port %s", args.port);
+    });
   })
   .catch(ex => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+    console.error(ex.stack);
+    process.exit(1);
+  });
