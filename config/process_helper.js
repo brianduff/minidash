f6df84c3c41044ddea625db2bc1c9b7a8099bc66
Brian Duff <cairndubh@gmail.com>
2019-05-29 15:24:11 -0700

    Invoke the updater

--- config/process_helper.js
+++ config/process_helper.js
@@ -1,4 +1,4 @@
-const { spawnSync } = require("child_process");
+const { spawnSync, execSync } = require("child_process");
 
 exports.exec = function(...args) {
   spawnSync(args[0], args.slice(1), {
@@ -10,5 +10,11 @@ exports.execInDir = function(dir, ...args) {
   spawnSync(args[0], args.slice(1), {
     stdio: "inherit",
     cwd: dir
+  });
+};
+
+exports.execInShell = function(...args) {
+  execSync(args[0], args.slice(1), {
+    stdio: "inherit"
   });
 };
--- github_updater.js
+++ github_updater.js
@@ -1,9 +1,16 @@
+const { execInShell } = require("config/process_helper");
+const os = require("os");
+const path = require("path");
+
 exports.onPush = function(req, res) {
   const event = req.header("X-Github-Event");
   const signature = req.header("X-Hub-Signature");
 
   if (event == "push") {
     console.log("Push from github detected. Redeploying");
+    execInShell(
+      path.join(os.homedir(), "minidash-deploy/minidash/config/update.js")
+    );
   } else {
     console.log("Unknown github event: %s", event);
   }
